2becbd6bfe567a6db3bae88e084187a25705665d
Paul Jones <pajones@adobe.com>
2023-07-11 12:11:07 -0600

    added websdk

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import { List } from './components/list';
 import { Admin } from './components/admin';
 
 function App() {
-  return (
-      <Routes>
-          <Route path="/" element={<List />} />
-          <Route path="/borrowed" element={<Admin/>} />
-      </Routes>
-  );
+    alloy("sendEvent", {
+        "xdm": {
+            "web": {
+                "webPageDetails": {
+                    "pageViews": {
+                        "value": 1
+                    },
+                    "name": "Game List",
+                    "isHomePage": true,
+                    "URL": window.location.href,
+                    "server": window.location.hostname
+                }
+            }
+        }
+    });
+    return (
+        <Routes>
+            <Route path="/" element={<List />} />
+            <Route path="/borrowed" element={<Admin/>} />
+        </Routes>
+    );
 }
 
 export default App;
